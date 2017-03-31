0c6a1e8c98a2fa4eb2754b2d5ce296e0cff0598b
MauveRanger <mpeschel@outlook.com>
2017-03-31 10:03:59 -0400

    Create EulerTest.java

---
+++ EulerTest.java
@@ -0,0 +1,27 @@
+//Created 31 March 2017
+//Just a place to keep my work together.
+
+public class EulerTest
+{
+	public static void main(String[] args)
+	{
+		System.out.println(_1());
+	}
+	
+	/**	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+		The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+	*/
+	public int _1()
+	{
+		int sum5 = 1000 - 5
+	}
+	
+	//Only guarunteed when both values are positive and divisor < limit.
+	public int sumOfMultiplesBelow(int limit, int divisor)
+	{
+		int maxit = (limit / divisor) * divisor;
+		if(maxit = divisor)
+			return maxit;
+		return ((long) maxit + divisor) * maxit / divisor;
+	}
+}
